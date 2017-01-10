5b87bcd010b4de1c51c8aeeba93aed9067f9a7ca
midhunhk <midhunhk@gmail.com>
2017-01-11 01:22:57 +0530

    fixes #4

--- app/src/main/java/com/ae/apps/tripmeter/activities/AboutActivity.java
+++ app/src/main/java/com/ae/apps/tripmeter/activities/AboutActivity.java
@@ -23,9 +23,13 @@
  */
 package com.ae.apps.tripmeter.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.ae.apps.tripmeter.R;
 
@@ -43,6 +47,26 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setHomeButtonEnabled(true);
-        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Navigate to GitHub Page
+        Button viewSourceCodeBtn = (Button) findViewById(R.id.viewSourceCode);
+        viewSourceCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = getString(R.string.github_source_visible_url);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        // Show the License
+        Button viewLicenseBtn = (Button) findViewById(R.id.viewLicense);
+        viewLicenseBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
