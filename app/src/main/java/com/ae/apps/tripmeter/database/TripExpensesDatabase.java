dcd0b14e938be53805d7ec0285c107ea73cb0467
midhunhk <midhunhk@gmail.com>
2017-01-24 23:40:03 +0530

    add licence

---
+++ app/src/main/java/com/ae/apps/tripmeter/database/TripExpensesDatabase.java
@@ -0,0 +1,10 @@
+package com.ae.apps.tripmeter.database;
+
+import com.ae.apps.common.db.DataBaseHelper;
+
+/**
+ * Created by user on 1/24/2017.
+ */
+
+public class TripExpensesDatabase extends DataBaseHelper {
+}
