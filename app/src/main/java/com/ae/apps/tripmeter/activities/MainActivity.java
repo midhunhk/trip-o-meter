65a4ec08cd122eaa69ce29bd036405b9bbe61180
midhunhk <midhunhk@gmail.com>
2017-03-15 09:42:21 +0530

    add licence to source files

--- app/src/main/java/com/ae/apps/tripmeter/activities/MainActivity.java
+++ app/src/main/java/com/ae/apps/tripmeter/activities/MainActivity.java
@@ -1,4 +1,4 @@
-/**
+/*
  * MIT License
  * <p>
  * Copyright (c) 2016 Midhun Harikumar
--- app/src/main/java/com/ae/apps/tripmeter/fragments/TripsListFragment.java
+++ app/src/main/java/com/ae/apps/tripmeter/fragments/TripsListFragment.java
@@ -1,4 +1,4 @@
-/**
+/*
  * MIT License
  * Copyright (c) 2016 Midhun Harikumar
  * Permission is hereby granted, free of charge, to any person obtaining a copy
