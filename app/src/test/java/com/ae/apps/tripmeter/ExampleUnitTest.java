5989ea2981eae0e22a1c80056b0c5a3498d3e0c7
Midhun Harikumar <midhunhk@gmail.com>
2017-05-11 13:28:46 +0530

    Update ExampleUnitTest.java

--- app/src/test/java/com/ae/apps/tripmeter/ExampleUnitTest.java
+++ app/src/test/java/com/ae/apps/tripmeter/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+    
+    @Test
+    public void multiplication_isCorrect() throws Exception {
+        assertEquals(4, 2 * 2);
+    }
 }
